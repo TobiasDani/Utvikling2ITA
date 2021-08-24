cfacb71cae9d210f3e2983f9c44b501adc7284e2
Tobias Danielsen <TobiasD@outlook.com>
2021-08-24 13:27:53 +0200

    Lagt til areal av trekant

--- MatteMedPython.py
+++ MatteMedPython.py
@@ -1,11 +1,21 @@
 
-#Regner ut arealet at firkanten
+#Regner ut arealet av en firekant
 
-høyde = int(input("\nHva skal høyden være? "))
-bredde = int(input("Hva skal bredden være? "))
+høyde = int(input("\nHva skal høyden av firekanten være? "))
+bredde = int(input("Hva skal bredden av firekanten være? "))
 
-areal = høyde * bredde
-omkrets = høyde + høyde + bredde + bredde
+areal = høyde*bredde
+omkrets = høyde+høyde+bredde+bredde
 
-print("\nArealet av firkanten er:", areal, "\nOmkretsen av firkanten er:", omkrets, "\n")
+print("\nArealet av firekanten er:", areal, "\nOmkretsen av firekanten er:", omkrets, "\n")
+
+
+#Regner ut arealet av en trekant
+
+høyde = int(input("Hva skal høyden av trekanten være? "))
+grunnlinje = int(input("Hva skal grunnlinja av trekanten være? "))
+
+areal = (høyde*bredde)/2
+
+print("\nArealet av trekanten er:", areal, "\n")
 
