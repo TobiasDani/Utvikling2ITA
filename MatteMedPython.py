08898fe02ae1f1ac0e7af85ba3b269a3b6fa85f9
Tobias Danielsen <TobiasD@outlook.com>
2021-08-24 13:16:49 +0200

    Firkant areal og omkrets regning

---
+++ MatteMedPython.py
@@ -0,0 +1,11 @@
+
+#Regner ut arealet at firkanten
+
+høyde = int(input("\nHva skal høyden være? "))
+bredde = int(input("Hva skal bredden være? "))
+
+areal = høyde * bredde
+omkrets = høyde + høyde + bredde + bredde
+
+print("\nArealet av firkanten er:", areal, "\nOmkretsen av firkanten er:", omkrets, "\n")
+
