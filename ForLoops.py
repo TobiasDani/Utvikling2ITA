2d9ae02d945597e41c6b951829a8d30f394d9fd0
Tobias Danielsen <TobiasD@outlook.com>
2021-08-30 11:10:48 +0200

    fix

--- ForLoops.py
+++ ForLoops.py
@@ -1,7 +1,41 @@
 
+def telle():
+    for i in range(1,71):
+        print(i)
+        if i == 35:
+            print("Du er halvveis")
 
-for i in range(1,71):
-    print(i)
-    if i == 35:
-        print("Du er halvveis")
+        if i % 7 == 0:
+            print('*')
+
+
+def fruktDef():
+    frukt = ["Eple", "Pære", "Sitron", "Melon"]
+
+    frukt.append("Plomme")
+
+    nfrukt = input("Skrive frukt: ")
+    frukt.append(nfrukt)
+
+    for i in frukt:
+        print(i)
+
+
+klasse = []
+nklasse = ""
+
+while nklasse != "Stop":
+    nklasse = input("\nSkriv navnet eller Stop hvis du er ferdig: ")
+    if nklasse != "Stop":
+        klasse.append(nklasse)
+        print("\n",nklasse, "er lagt til")
+
+klasse.sort()
+
+print("\nKlassen består av:")
+
+for navn in klasse:
+    print("", navn)
+
+exit()
 
