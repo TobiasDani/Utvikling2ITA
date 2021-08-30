de8a88a4912c54b3346333c1b9de20e8020ce5d7
Tobias Danielsen <TobiasD@outlook.com>
2021-08-30 10:16:58 +0200

    fix

---
+++ Terningkast.py
@@ -0,0 +1,21 @@
+import random
+
+
+def terningkast():
+    terning1 = 0
+    terning2 = 0
+    summen = 0
+
+    while summen !=12:
+        input("\nTrykk på en tast: ")
+        terning1 = random.randint(1,6)
+        terning2 = random.randint(1,6)
+        print("\nTerning 1 =", terning1)
+        print("\nTerning 2 =", terning2)
+        summen = terning1 + terning2
+        print("\nSummen er lik", summen)
+
+    print("Hurra!")
+
+terningkast()
+
