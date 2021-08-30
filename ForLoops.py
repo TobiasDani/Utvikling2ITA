368fd88d7a58d61e0d7a8491c8ec9e49cb2aa68b
Tobias Danielsen <TobiasD@outlook.com>
2021-08-30 10:17:07 +0200

    fix

--- ForLoops.py
+++ ForLoops.py
@@ -1,20 +1,6 @@
-import random
 
 
-def terningkast():
-    terning1 = 0
-    terning2 = 0
-    summen = 0
-
-    while summen !=12:
-        input("Trykk på en tast: ")
-        terning1 = random.randint(1,6)
-        terning2 = random.randint(1,6)
-        print("Terning 1 =", terning1)
-        print("Terning 2 =", terning2)
-        summen = terning1 + terning2
-        print("Summen er lik", summen)
-
-    print("Hurra!")
-
-terningkast()
+for i in range(1,71):
+    print(i)
+    if i == 35:
+        print("Du er halveis")
